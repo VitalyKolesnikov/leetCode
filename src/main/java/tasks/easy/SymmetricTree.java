76753ddf69b05f14b02e8d3a1fb3a66aa875a237
Vitaly Kolesnikov <vkolesnikov@custis.ru>
2022-05-29 16:59:00 +0300

    SymmetricTree

---
+++ src/main/java/tasks/easy/SymmetricTree.java
@@ -0,0 +1,45 @@
+package tasks.easy;
+
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+
+        return (left.val == right.val &&
+                isMirror(left.left, right.right) &&
+                isMirror(left.right, right.left));
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
