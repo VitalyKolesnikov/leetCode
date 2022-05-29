7620d412c00ba0155ad452455c4a1401caea3d65
Vitaly Kolesnikov <vkolesnikov@custis.ru>
2022-05-29 17:54:25 +0300

    ReverseLinkedList

---
+++ src/main/java/tasks/easy/ReverseLinkedList.java
@@ -0,0 +1,33 @@
+package tasks.easy;
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
